2ef06261715002dca82b928430e4b88b5e528e32
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-06-22 17:40:15 +0100

    StillOnNavBar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import style from './navbar.css'
+import { HiShoppingCart } from "react-icons/hi";
+import {FaUser} from "react-icons/fa";
+
 
 const Navbar = () => {
     return (
         <div className={style.navbar}>
-            <div className={nav-logo}>
+            <div className={style.navLogo}>
+                <p>Shopper</p>
+                <ul className={style.navMenu}>
+                    <li>Home</li>
+                    <li>New</li>
+                    <li>Iphone</li>
+                    <li>Samsung</li>
+                    <li>About us</li>
+                    <li>Contact</li>
+                </ul>
+                <div className={style.navLoginCart}>
+                    <button>Login</button>
+                    <HiShoppingCart className="icon"/>
+                </div>
 
             </div>
         </div>
