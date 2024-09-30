36ab5d96175199099eacf0d4feeff73d4303d213
Abolaji2K21 <151364721+Abolaji2K21@users.noreply.github.com>
2024-09-30 13:29:22 +0100

    Refactoring done

--- src/pages/HeroPage.jsx
+++ src/pages/HeroPage.jsx
@@ -24,16 +24,15 @@ const HeroPage = () => {
         <div className="flex flex-col w-full md:w-1/2 p-8 md:p-20 order-2 md:order-1">
           <h1 className="text-4xl md:text-5xl font-bold mb-4 text-gray-800 font-serif">New <br /> collection</h1>
           <p className="px-5 pb-6 text-lg leading-7 text-[#777777]">
-          The highest quality products, sewn in Ireland from Irish materials. Quality and durability for years.
-          </p>
+              Don't miss out on owning the latest in mobile technology. Shop now and elevate your everyday experience!          </p>
           <div className={`flex flex-col md:flex-row transition-all duration-500`}>
             <button
               className="bg-[#e32c2c] text-white hover:bg-[#e25151] font-bold py-2 px-4 rounded mb-4 md:mb-0 md:mr-4"
-              aria-label="Donate now"
-              title="Donate now"
+              aria-label="Purchase now"
+              title="Purchase now"
             //   onClick={() => setModalOpen(true)}
             >
-              Donate now
+                Purchase now
             </button>
           </div>
         </div>
